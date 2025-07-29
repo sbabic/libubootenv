sbabic/libubootenv

commit c478e8d92bd7676a249693f7d132bed7b17ae158

uboot_env: Emulate %ms in sscanf()
Signed-off-by: Christian Storm <christian.storm@siemens.com>

src/uboot_env.c
@@ -1258,13 +1258,25 @@ int libuboot_read_config_ext(struct uboot_ctx **ctxlist, const char *config)
 	ctx->size = 0;
 	rewind(fp);
 
-	while (getline(&line, &bufsize, fp) != -1) {
+	int len;
+	while ((len = getline(&line, &bufsize, fp)) != -1) {
 		/* skip comments */
 		if (line[0] == '#')
 			continue;
 
+#if defined(__FreeBSD__)
+		/*
+		 * POSIX.1-2008 introduced the dynamic allocation conversion
+		 * specifier %m which is not implemented on FreeBSD.
+		 */
+		tmp = calloc(1, len + 1);
+		ret = sscanf(line, "%s %lli %zx %zx %lx %d",
+				tmp,
+#else
+		(void)len;
 		ret = sscanf(line, "%ms %lli %zx %zx %lx %d",
 				&tmp,
+#endif
 				&dev->offset,
 				&dev->envsize,
 				&dev->sectorsize,
@@ -1274,13 +1286,16 @@ int libuboot_read_config_ext(struct uboot_ctx **ctxlist, const char *config)
 		/*
 		 * At least name offset and size should be set
 		 */
-		if (ret < 3 || !tmp)
+		if (ret < 3) {
+			free(tmp);
 			continue;
+		}
 
 		/*
 		 * If size is set but zero, entry is wrong
 		 */
 		if (!dev->envsize) {
+			free(tmp);
 			retval = -EINVAL;
 			break;
 		}

commit 5e6d8b6318be809ac5a8989756ade45ece88ca7e

libubootenv: fix segfault due to uninitialized pointer in config parser
Issue: The issue was introduced in commit c478e8d9, which replaced the use of %ms in sscanf() with a calloc()-based workaround for platforms where %ms is not supported (e.g., FreeBSD).
However, this change inadvertently introduced a logic flaw: it uses calloc to emulate %ms% in sscanf() but also added 2 free instructions which could lead to free non-allocated memory on tmp when sscanf()
is used to perform the dynamic allocation.

Fix: Ensure `tmp` is initialized to NULL before each call to sscanf with `%ms`
in `libuboot_read_config_ext()`. This prevents `free(tmp)` from crashing
when sscanf fails to allocate memory (e.g., due to malformed config lines).

Fixes segmentation fault observed when running swupdate with a ubootenv config file.

Signed-off-by: Mohamed-nour Toumi <mohamed.toumi_ext@softathome.com>

src/uboot_env.c
@@ -871,6 +871,7 @@ int libuboot_read_config_ext(struct uboot_ctx **ctxlist, const char *config)
 				tmp,
 #else
 		(void)len;
+		tmp = NULL;
 		ret = sscanf(line, "%ms %lli %zx %zx %lx %d",
 				&tmp,
 #endif